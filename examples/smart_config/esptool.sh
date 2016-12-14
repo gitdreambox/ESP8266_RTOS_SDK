4554bc48ba9bd20666606daa30b3fb607500f21d
435398366@qq.com <435398366@qq.com>
2016-12-14 11:32:09 +0800

    修改esptool 下载波特率为1500000  ，加快下载速度

---
+++ examples/smart_config/esptool.sh
@@ -0,0 +1,1 @@
+esptool.py -b 1500000 --port /dev/ttyUSB0 write_flash 0x00000 ./../bin/boot_v1.6.bin 0x1000 ./../bin/upgrade/user1.4096.new.6.bin 0x3fc000 ./../bin/esp_init_data_default.bin 0x3fe000 ./../bin/blank.bin
